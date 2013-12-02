e97eddf6d6d73a2748a17ccb696d15529c07da4e
Björn Esser <bjoern.esser@gmail.com>
2013-12-02 18:04:03 +0100

    adaption for new SHOGUN-namespace-defines

--- src/shogun/structure/PrimalMosekSOSVM.cpp
+++ src/shogun/structure/PrimalMosekSOSVM.cpp
@@ -8,7 +8,7 @@
  * Copyright (C) 2012 Fernando José Iglesias García
  */
 
-#ifdef USE_MOSEK
+#ifdef SHOGUN_USE_MOSEK
 
 #include <lib/DynamicObjectArray.h>
 #include <lib/List.h>
@@ -285,4 +285,4 @@ void CPrimalMosekSOSVM::set_epsilon(float64_t epsilon)
 	m_epsilon = epsilon;
 }
 
-#endif /* USE_MOSEK */
+#endif /* SHOGUN_USE_MOSEK */
